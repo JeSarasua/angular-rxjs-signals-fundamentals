e71de42c85595df21766e690197f51ef4444a3ea
Jeremy Sarasua <jeremy.sarasua@aveva.com>
2024-08-13 10:29:36 -0700

    undo

--- apm-begin/src/main.ts
+++ apm-begin/src/main.ts
@@ -1,5 +1,3 @@
-// Hello World
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
